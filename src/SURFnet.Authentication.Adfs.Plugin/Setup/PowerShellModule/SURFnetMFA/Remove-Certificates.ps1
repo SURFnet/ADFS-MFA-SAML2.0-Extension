4b0aea8dcc16bef681d1d689eb211b7407bb724f
Pieter-Paul Luijten <ppluijten@gmail.com>
2019-12-10 15:43:47 +0100

    Formatting.

--- src/SURFnet.Authentication.Adfs.Plugin/Setup/PowerShellModule/SURFnetMFA/Remove-Certificates.ps1
+++ src/SURFnet.Authentication.Adfs.Plugin/Setup/PowerShellModule/SURFnetMFA/Remove-Certificates.ps1
@@ -1,22 +1,22 @@
-﻿#####################################################################
-#Copyright 2017 SURFnet bv, The Netherlands
+﻿###########################################################################
+# Copyright 2017 SURFnet bv, The Netherlands
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
-#####################################################################
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###########################################################################
 
 $ErrorActionPreference = "Stop"
 
-function Remove-Certificates{
+function Remove-Certificates {
     Param(
         [string]
         $SfoCertificateThumbprint,
@@ -24,12 +24,12 @@ function Remove-Certificates{
         $ServiceProviderCertificateThumbprint
     )    
 	
-    if($SfoCertificateThumbprint){
+    if ($SfoCertificateThumbprint) {
         Remove-Item -Path cert:\localMachine\my\$SfoCertificateThumbprint
         Write-Host -ForegroundColor Green "Removed SFO certificate with thumbprint $SfoCertificateThumbprint"
     }
 
-    if($ServiceProviderCertificateThumbprint){
+    if ($ServiceProviderCertificateThumbprint) {
         Remove-Item -Path cert:\localMachine\my\$ServiceProviderCertificateThumbprint
         Write-Host -ForegroundColor Green "Removed signing certificate with thumbprint $ServiceProviderCertificateThumbprint"
     }
