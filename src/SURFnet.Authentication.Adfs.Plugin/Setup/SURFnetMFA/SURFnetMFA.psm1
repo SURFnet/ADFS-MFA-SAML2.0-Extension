ec957952d5a78e32ce24098a1e94875afd932718
Johan Hoek <Johan.Hoek@Winvision.nl>
2020-03-12 11:23:58 +0100

    Removed old powershell files

--- src/SURFnet.Authentication.Adfs.Plugin/Setup/SURFnetMFA/SURFnetMFA.psm1
+++
@@ -1,3 +0,0 @@
-﻿$funcs = Get-ChildItem -Recurse (Join-Path $PSScriptRoot *.ps1)
-$funcs | ForEach-Object { . $_.FullName; }
-Export-ModuleMember -Function $($funcs | Select-Object -ExpandProperty BaseName)
