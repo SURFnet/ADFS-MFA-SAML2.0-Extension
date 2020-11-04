0043dd8ff2c9d99ce676caa1d910e6914d8f8b4f
Juri van Tuijl <juri.vantuijl@winvision.nl>
2020-11-04 13:22:32 +0100

    StrongNameSigner version update

--- SolutionItems/SignSustainsys.cmd
+++ SolutionItems/SignSustainsys.cmd
@@ -1,2 +1,2 @@
-..\packages\Brutal.Dev.StrongNameSigner.2.1.0\build\StrongNameSigner.Console.exe -a ..\packages\Sustainsys.Saml2.2.7.0\lib\net461\Sustainsys.Saml2.dll -k SURFnet.Authentication.Adfs.Plugin.snk
+..\packages\Brutal.Dev.StrongNameSigner.2.7.1\build\StrongNameSigner.Console.exe -a ..\packages\Sustainsys.Saml2.2.7.0\lib\net461\Sustainsys.Saml2.dll -k SURFnet.Authentication.Adfs.Plugin.snk
 pause
