ca64b81c961ab29f0fc3cecd864d2cbc40ea773d
Pieter-Paul Luijten <ppluijten@gmail.com>
2022-08-12 12:36:58 +0200

    Removing unused methods

--- src/Setup/Messages.cs
+++ src/Setup/Messages.cs
@@ -1,28 +1,12 @@
-﻿using SURFnet.Authentication.Adfs.Plugin.Setup.Question;
+﻿using System;
+
+using SURFnet.Authentication.Adfs.Plugin.Setup.Question;
 using SURFnet.Authentication.Adfs.Plugin.Setup.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SURFnet.Authentication.Adfs.Plugin.Setup
 {
     public static class Messages
     {
-        public static bool ReallyXyz()
-        {
-            LogService.WriteWarning("   There is a registration in the ADFS configuration of an SFO MFA extension.");
-            LogService.WriteWarning("   In general, it is not a good idea to remove this registration.");
-            LogService.WriteWarning("   This ADFS server will then produce loading errors in the ADFS EventLog.");
-            LogService.WriteWarning("   ");
-            LogService.WriteWarning("   However, if you want to immediately (re)install a version of the SFO MFA extension.");
-            LogService.WriteWarning("   However, IFF you want to immediately (re)install a version of the SFO MFA extension.");
-            LogService.WriteWarning("   Then it could be OK (as cleanup first).");
-            Console.WriteLine();
-            return false;
-        }
-
         public static ReturnOptions EndWarning(string msg)
         {
             LogService.Log.Warn("EndWarning: " + msg);
@@ -48,16 +32,21 @@ namespace SURFnet.Authentication.Adfs.Plugin.Setup
             bool doit;
 
             LogService.Log.Warn(question);
-            if ('y' == AskYesNo.Ask(question,true))
+            if ('y' == AskYesNo.Ask(question, true))
             {
                 doit = true;
             }
             else
             {
-                if ( noMessage== null ) noMessage = "OK. Will not do it.";
+                if (noMessage == null)
+                {
+                    noMessage = "OK. Will not do it.";
+                }
+
                 LogService.WriteWarning(noMessage);
                 doit = false;
             }
+
             Console.WriteLine();
 
             return doit;
