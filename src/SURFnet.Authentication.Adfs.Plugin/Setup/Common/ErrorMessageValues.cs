09920a3288b79ddc365cc026961ad374d70a6d5a
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2023-07-20 17:42:19 +0200

    Update user error messages

--- src/SURFnet.Authentication.Adfs.Plugin/Setup/Common/ErrorMessageValues.cs
+++ src/SURFnet.Authentication.Adfs.Plugin/Setup/Common/ErrorMessageValues.cs
@@ -5,12 +5,20 @@
         /// <summary>
         /// The default error message resourcer identifier.
         /// </summary>
+        /// Message: An unexpected error occured. Please try again.
         public const string DefaultErrorMessageResourcerId = "ERROR_0000";
 
         /// <summary>
-        /// The default verification failed resourcer identifier.
+        /// The default authentication failed resourcer identifier.
         /// </summary>
+        /// Message: The additional authentication failed.
         public const string DefaultVerificationFailedResourcerId = "ERROR_0001";
+
+        /// <summary>
+        /// There is a configuration issue with the plugin itself
+        /// </summary>
+        /// Message: The configuration of this MFA extension is not correct
+        public const string PluginConfigurationErrorResourceId = "ERROR_0002";
 
         /// <summary>
         /// The default verification failed resourcer identifier.
